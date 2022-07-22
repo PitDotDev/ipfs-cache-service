6cb153b9dd0628e4ef18b398365a7c7f7dd426c7
SixStringer91 <danlootsev@gmail.com>
2022-07-22 05:13:30 +0300

    add proxy

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const path = require('path')
 class Config {
     constructor(fname) {
         this.Debug = true
-        this.Port = 14000
+        this.Port = 13000
         this.Secret = "secret"
         // this.CID = 'fda210a4af51fdd2ce1d2a1c0307734ce6fef30b3eec4c04c4d7494041f2dd10'
         // this.ShaderFile = 'app.wasm'
---
+++ proxy/proxy.js
@@ -0,0 +1,67 @@
+const cors = require('cors');
+const express = require('express');
+const request = require('request');
+
+const PORT = 14000;
+
+const TIMEOUT = 5000;
+
+const networks = new Map()
+    .set('dappnet', 'http://127.0.0.1')
+    .set('masternet', 'http://3.209.99.179')
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (_, res) => {
+    res.send('beam-proxi is running!');
+})
+
+app.get('/ipfs/:network/:id', (req, res) => {
+    const currentNetwork = networks.get(req.params.network) || networks.get('masternet');
+    const options = {
+        uri: [`${currentNetwork}:8070`, 'ipfs', req.params.id].join('/'),
+        encoding: null,
+        timeout: TIMEOUT
+    };
+    request(options,
+        function (err, _, body) {
+            try {
+                if (err) {
+                    return res.status(404)
+                        .set('Content-Type', 'application/json')
+                        .send({ message: err.code === 'ESOCKETTIMEDOUT' ? 'hash not found' : 'invalid hash' })
+                }
+                return res
+                    .set('Content-Type', 'application/octet-stream')
+                    .send(body);
+            } catch (error) {
+                console.log(error);
+            }
+
+        });
+});
+
+
+app.get('/repo/:network/:key', (req, res) => {
+    const currentNetwork = networks.get(req.params.network) || networks.get('masternet');
+    console.log([currentNetwork, `repo?key=${req.params.key}`].join('/'));
+    request(
+        { uri: [`${currentNetwork}:13000`, `repo?key=${req.params.key}`].join('/') },
+        function (err, response, body) {
+            if (err) return res.status(500)
+                .set('Content-Type', 'application/json')
+                .send({ message: 'server error' });
+
+            return res.status(response.statusCode)
+                .set('Content-Type', 'application/json')
+                .send(body);
+        });
+})
+
+//
+
+app.listen(PORT, () =>
+    console.log(`Beam-proxy is running on http://localhost:${PORT}`)
+);
