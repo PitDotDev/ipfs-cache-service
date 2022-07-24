98e3206c301755d50e1f912e05f95723291e7c93
SixStringer91 <danlootsev@gmail.com>
2022-07-25 02:27:56 +0300

    add timeout on get data

--- sourc3/repo.js
+++ sourc3/repo.js
@@ -74,9 +74,11 @@ class Repo {
     async __continue_pin() {
         const gitHash = this.__last_key();
         logger(`${this._title} repo-${this._id} ${gitHash} get data`);
+        const timeoutId = setTimeout(() => this.__continue_pin(), 20000);
         if (gitHash) {
             this._api.contract(`cid=${this._cid},role=user,action=repo_get_data,repo_id=${this._id},obj_id=${gitHash}`,
                 (err, { object_data }) => {
+                    clearTimeout(timeoutId);
                     logger(`${this._title} repo-${this._id} ${gitHash} data recieved`);
 
                     if (err) {
